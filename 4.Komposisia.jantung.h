74493e01d669b879017d7f3e3c3572602792b7eb
xybayue-tbk <xybayue@gmail.com>
2026-05-11 11:43:32 +0700

    class jantung dan isinya

--- 4.Komposisia.jantung.h
+++ 4.Komposisia.jantung.h
@@ -2,3 +2,14 @@
 #define JANTUNG_H
 
 using namespace std;
+
+class Jantung {
+    public:
+    Jantung() {
+        cout << "Jantung dibuat" << endl;
+    }
+    ~Jantung() {
+        cout << "Jantung dihancurkan" << endl;
+    }
+};
+#endif
