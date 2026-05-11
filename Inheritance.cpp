78a91979cb622aa16d1276a26733032648183efa
xybayue-tbk <xybayue@gmail.com>
2026-05-11 11:32:19 +0700

    menambahkan class orang dan isinya

--- Inheritance.cpp
+++ Inheritance.cpp
@@ -1,3 +1,20 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class orang {
+public:
+    string nama;
+
+    orang(string pnama) : nama(pnama) {
+        cout << "orang di buat\n" << endl;
+    }
+
+    ~orang() {
+        cout << "orang dihapus\n" << endl;
+    }
+
+    int jumlah(int a, int b) {
+        return a + b;
+    }
+};
