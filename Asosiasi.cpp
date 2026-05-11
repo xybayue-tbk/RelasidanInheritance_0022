f7bdd35ebaa263fca99b70589b86267f069ed72a
xybayue-tbk <xybayue@gmail.com>
2026-05-11 11:12:29 +0700

    menambahkan variable,constructor dan destructor

--- Asosiasi.cpp
+++ Asosiasi.cpp
@@ -4,4 +4,13 @@ using namespace std;
 
 class dokter;
 class pasien {
+public:
+    string nama;
+    vector<dokter*> daftar_dokter;
+    pasien(string pnama) :nama(pnama) {
+        cout << "Pasien \"" << nama << "\" ada\n";
+    }
+    ~pasien() {
+        cout << "Pasien \"" << nama << "\" tidak ada\n";
+    }
 };
