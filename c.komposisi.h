72dd3b65fcd8ce973a38169ec746016d64e8fa7a
xybayue-tbk <xybayue@gmail.com>
2026-05-11 11:54:21 +0700

    selesai

---
+++ c.komposisi.h
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include "4.komposisi.jantung.h"
+#include "4.komposisi.manusia.h"
+
+int main() {
+    manusia *varmanusia = new manusia("markus");
+}
