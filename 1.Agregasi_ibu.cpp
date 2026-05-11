b1c5fa605bb27df8c9afae392b0f97d00f36f3ca
xybayue-tbk <xybayue@gmail.com>
2026-05-11 10:44:00 +0700

    deklarasi fungsi anggota di dalam class

--- 1.Agregasi_ibu.cpp
+++ 1.Agregasi_ibu.cpp
@@ -6,11 +6,13 @@ class ibu {
 public:
     string nama;
     vector<anak*> daftar_anak;
-    
+
     ibu(string pnama) :nama(pnama) {
         cout << "Ibu \"" << nama << "\" ada\n";
     }
     ~ibu() {
         cout << "Ibu \"" << nama << "\" tidak ada\n";
     }
+    void tambahanak(anak*);
+    void cetakanak();
 };
