3fe22ab189592b64c7cbd125d8d1cd50eb1d9bfb
xybayue-tbk <xybayue@gmail.com>
2026-05-11 15:14:57 +0700

    membuat class user

--- praktikum.cpp
+++ praktikum.cpp
@@ -1,3 +1,22 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+
+class member;
+class user {
+protected:
+    static int globalId;
+public:
+    int id;
+    string nama;
+    string email;
+
+    user(string pnama, string pemail) : nama(pnama), email(pemail)
+    {
+        id = generateId();
+    }
+
+    int generateId() {
+        return ++globalId;
+    }
+};
