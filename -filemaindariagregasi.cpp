74ff158a2cbc3b8fb429ac3e43a46b9909a84b89
xybayue-tbk <xybayue@gmail.com>
2026-05-11 11:05:09 +0700

    menambahkan main progam dan isinya

--- -filemaindariagregasi.cpp
+++ -filemaindariagregasi.cpp
@@ -2,3 +2,28 @@
 using namespace std;
 #include "1.Agregasi_ibu.h"
 #include "1.Agregasi_anak.h"
+
+int main() {
+    ibu* varibu = new ibu("dini");
+    ibu* varibu2 = new ibu("novi");
+    anak* varanak1 = new anak("tono");
+    anak* varanak2 = new anak("rini");
+    anak* varanak3 = new anak("dewi");
+
+    varibu->tambahanak(varanak1);
+    varibu->tambahanak(varanak2);
+    varibu2->tambahanak(varanak3);
+    varibu2->tambahanak(varanak1);
+
+    varibu->cetakanak();
+    varibu2->cetakanak();   
+
+    delete varibu;
+    delete varibu2;
+    delete varanak1;
+    delete varanak2;
+    delete varanak3;
+
+    system("pause");
+    return 0;  
+}
