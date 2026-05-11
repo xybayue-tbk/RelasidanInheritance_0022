95b32fa7dd9c69fcb9806cb2a211cf426e701003
xybayue-tbk <xybayue@gmail.com>
2026-05-11 11:50:12 +0700

    class manusia

--- 4.Komposisia.manusia.h
+++ 4.Komposisia.manusia.h
@@ -2,3 +2,15 @@
 #define MANUSIA_H
 using namespace std;
 
+class manusia {
+public:
+    string nama;
+    jantung varjantung;
+    manusia(string pnama) :nama(pnama) {
+        cout << nama << "hidup\n"
+    }
+    ~manusia() {
+        cout << nama << "mati\n"
+    }
+};
+#endif
