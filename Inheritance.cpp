8ceb005909677a482b87c790b43b2158b0a96865
xybayue-tbk <xybayue@gmail.com>
2026-05-11 11:39:09 +0700

    menambahkan class pelajar dan int main

--- Inheritance.cpp
+++ Inheritance.cpp
@@ -18,3 +18,29 @@ public:
         return a + b;
     }
 };
+
+class pelajar : public orang {
+public:
+    string sekolah;
+
+    pelajar(string pnama, string psekolah) : orang(pnama), sekolah(psekolah) {
+        cout << "pelajar di buat\n" << endl;
+    }
+
+    ~pelajar() {
+        cout << "pelajar dihapus\n" << endl;
+    }
+
+    string perkenalan() {
+        return "nama saya " + nama + " dan saya sekolah di " + sekolah;
+    }
+};
+
+int main() {
+
+    pelajar siswa1("andi laksono", "SMA 1 Bantul");
+    cout << siswa1.perkenalan() << endl;
+    cout << "hasil = " << siswa1.jumlah(10, 90) << endl;
+
+    return 0;
+}
